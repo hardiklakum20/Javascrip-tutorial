17c0e21ad3828469c4fac5f931aa5042e53b184e
hardiklakum20 <120251398+hardiklakum20@users.noreply.github.com>
2024-08-12 15:13:49 +0530

    class and inheritance
    class and inheritance

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,32 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`User Name is ${this.username}`);        
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);        
+    }
+}
+
+const chai = new Teacher("Chai", "chai@gmail.com", "123")
+
+chai.addCourse()
+chai.logMe()
+
+const masalaChai = new User("Masala Chai")
+
+masalaChai.logMe()
+
+console.log(chai instanceof User);
+
